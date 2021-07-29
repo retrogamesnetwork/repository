c9645553da08e1c20a84f8eda74a6c6ee9b60ef6
Alexander Guryanov <caiiiycuk@gmail.com>
2021-07-29 09:34:13 +0700

    datafiles under details

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { initSearch } from "./search";
 
-initSearch();
+function init() {
+	initDatafilesViewIfNeeded();
+	initSearch();
+}
+
+function initDatafilesViewIfNeeded() {
+	if ((window.location.search || "").indexOf("datafiles=true") >= 0) {
+		const el = document.getElementById("datafiles") as HTMLDetailsElement | null;
+		if (el) {
+			el.open = true;
+		}
+	}
+}
+
+init();
