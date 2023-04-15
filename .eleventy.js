886aab325ea47a6b8aaabcdd5c01ffc970d56043
Alexander Guryanov <caiiiycuk@gmail.com>
2023-04-15 19:10:20 +0200

    reenable mutiplayer

--- .eleventy.js
+++ .eleventy.js
@@ -150,16 +150,14 @@ module.exports = function(config) {
         Mobile
     </a>
 </div>
-<!--
 <div class="ml-2 my-2 rounded-xl border-2 border-purple-400 bg-purple-100 cursor-pointer">
-    <a class="font-bold text-lg text-purple-800 px-2 flex flex-row items-center" href="/multiplayer/">
+    <a class="font-bold text-lg text-purple-800 px-2 flex flex-row items-center" href="https://dos.zone/en/multiplayer/">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-purple-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         Multiplayer
     </a>
 </div>
--->
             `;
         } else {
             for (let i = 0; i < rows[row].length; ++i) {
